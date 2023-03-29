7c1ea0d488bfebe02bc080c3f5932dc885336076
andreas_mlv <odinokiigero@gmail.com>
2023-03-29 10:41:14 +0300

    Added main function.

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ def get_path(filename=None):
 
 def read_file(filename):
     try:
-        with open(get_path(filename), 'r') as f:
+        with open(get_path(filename), 'r', encoding="utf8") as f:
             return f.read()
     except:
         return None
@@ -40,4 +40,25 @@ def count_lines(text):
         splitters_count += text.count(splitter)
     return splitters_count + 1
 
+def calculate(text):
+    words_count = count_words(text)
+    lines_count = count_lines(text)
+    return {
+        "words": words_count,
+        "lines": lines_count,
+    }
+
+def generate_results(results):
+    words = results.get("words")
+    lines = results.get("lines")
+    return f"Words: {words}, Lines: {lines}"
+
+if __name__ == "__main__":
+    text = read_file('example_text.txt')
+    calculated = calculate(text)
+    results = generate_results(calculated)
+    print(results)
+    
+    # Output: 
+    # Words: 201, Lines: 10
 
